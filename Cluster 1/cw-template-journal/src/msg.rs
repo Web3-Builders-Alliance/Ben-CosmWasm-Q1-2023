68a40a24b424621511a98d74be5d98f24d48e733
Ben | Monarch <bgoober2@protonmail.com>
2023-02-09 05:11:23 -0500

    generate additional template contract for testing, edits to template journal
    Signed-off-by: Ben | Monarch <bgoober2@protonmail.com>

--- Cluster 1/cw-template-journal/src/msg.rs
+++ Cluster 1/cw-template-journal/src/msg.rs
@@ -4,9 +4,7 @@ use cosmwasm_schema::{cw_serde, QueryResponses};
 pub struct InstantiateMsg {
     pub count: u128,
     // add owner to the instantiate message
-    pub owner: Addr,
-    // add user_count to the instantiate message
-    pub user_count: Map<Addr, u128>,
+    pub owner: String, // an Addr is expected, but we will validate it as an address and return it
 }
 
 #[cw_serde]
@@ -28,7 +26,7 @@ pub enum QueryMsg {
     GetOwner {},
 
     #[returns(GetUserCountResponse)]
-    GetUserCount { user: Addr, user_count: u128 }, // return an Addr and the count for that Addr as a u128
+    GetUserCount { user: String, user_count: u128 }, // return an Addr that was an address-validated String and the count for that Addr as a u128
 }
 
 // We define a custom struct for each query response
@@ -40,12 +38,13 @@ pub struct GetCountResponse {
 // define GetOwnerResponse
 #[cw_serde]
 pub struct GetOwnerResponse {
-    pub owner: Addr, // return an Addr, not just a String
+    pub owner: String, // return an Addr, not just a String
 }
 
 // define GetUserCountResponse
 #[cw_serde]
 pub struct GetUserCountResponse {
-    pub user: Addr,
+    pub user: String,
+    // validate it as an address
     pub user_count: u128,
 }
