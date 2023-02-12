6128e54cd7838084c181eeed569d18129162c900
Ben | Monarch <bgoober2@protonmail.com>
2023-02-11 21:56:57 -0500

    generate cw-template for proposed changes to the contract. Run cargo commands for schema and tests, and release build
    Signed-off-by: Ben | Monarch <bgoober2@protonmail.com>

---
+++ Cluster 2/cw-template-changes/src/state.rs
@@ -0,0 +1,12 @@
+use cosmwasm_std::Addr;
+use cw_storage_plus::Item;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct State {
+    pub count: i32,
+    pub owner: Addr,
+}
+
+pub const STATE: Item<State> = Item::new("state");
